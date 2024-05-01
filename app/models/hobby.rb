9467b4d4f94f9ea831fa4e2435c0cfac05a1331e
inunokimoti <tc.202402.eri.nakamura@gmail.com>
2024-05-01 19:36:08 +0900

    バリテーションの設定

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,22 +1,20 @@
 class Hobby < ApplicationRecord
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :city, presence: true
-  validates :number, presence: true
-  validates :phone, format: { with: /\A\d{10,11}\z/, message: "is 10 or 11 digits only" }
-  validates :birthday, presence: true
-  validates :skill, presence: true
-  validates :profile, presence: true
-       
-  has_many :hobbies
+
+  with_options presence: true do
+    validates :type
+    validates :text
+    validates :user_id
+  end
+
+  belongs_to :user
   has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :where
+  belongs_to :category
+  belongs_to :count
   belongs_to :from
 
-  validates :from_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :where_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :where_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
